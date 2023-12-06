b3d649aa40efa99bac15608cb29d091417ef679b
Дмитрий Горбунов <bous07@mail.ru>
2023-12-06 20:20:50 +0300

    Add Shared

---
+++ ChatAppWithSignalR.Shared/AuthenticateResponse.cs
@@ -0,0 +1,11 @@
+﻿using ChatAppWithSignalR.Shared.Base;
+
+namespace ChatAppWithSignalR.Shared
+{
+    public class AuthenticateResponse : BaseResponse
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Token { get; set; }
+    }
+}
